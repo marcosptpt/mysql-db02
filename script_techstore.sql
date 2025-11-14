ebec99049de12d05f91daf0ffd532964756f5649
marcosptpt <mf95pt@gmail.com>
2025-11-14 15:46:28 +0000

    Update script_techstore.sql

--- script_techstore.sql
+++ script_techstore.sql
@@ -1,37 +1,43 @@
 -- Criar Base de Dados
-CREATE DATABASE techstore;
+CREATE DATABASE IF NOT EXISTS techstore;
 
 USE techstore;
 
 -- Criar tabela clientes
-CREATE TABLE clientes(
-	id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     localidade VARCHAR(20),
     telefone VARCHAR(20)
 );
 
 -- Criar tabela produtos
-CREATE TABLE produtos(
-	id_produto INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS produtos (
+    id_produto INT AUTO_INCREMENT PRIMARY KEY,
     designacao VARCHAR(50) NOT NULL,
     preco FLOAT(5) NOT NULL
 );
 
 -- Criar tabela vendas
-CREATE TABLE vendas(
-	id_venda INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS vendas (
+    id_venda INT AUTO_INCREMENT PRIMARY KEY,
     data_venda DATE NOT NULL,
-    id_cliente INT NOT NULL, -- tem de ser do mesmo tipo do da tabela clientes, senão ao fazer ligação ia dar erro
-	FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+    id_cliente INT NOT NULL,
+    CONSTRAINT fk_vendas_clientes
+        FOREIGN KEY (id_cliente)
+        REFERENCES clientes(id_cliente)
 );
 
--- Criar tabela vendas linha
-CREATE TABLE vendas_linha(
-	id_linha_venda INT AUTO_INCREMENT PRIMARY KEY,
+-- Criar tabela vendas_linha
+CREATE TABLE IF NOT EXISTS vendas_linha (
+    id_linha_venda INT AUTO_INCREMENT PRIMARY KEY,
     id_venda INT NOT NULL,
     id_produto INT NOT NULL,
     quantidade INT,
-    FOREIGN KEY (id_venda) REFERENCES vendas(id_venda),
-    FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
+    CONSTRAINT fk_linha_venda
+        FOREIGN KEY (id_venda)
+        REFERENCES vendas(id_venda),
+    CONSTRAINT fk_linha_produto
+        FOREIGN KEY (id_produto)
+        REFERENCES produtos(id_produto)
 );
